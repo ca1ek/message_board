514f0b26e2fa0d568a30fa00f12f6ca48b1c2e5d
ca1ek <ca1ek@users.noreply.github.com>
2015-11-21 16:01:19 +0100

    Added ThreadForm

--- forms.py
+++ forms.py
@@ -1,7 +1,12 @@
 ﻿from django import forms
-from .models import Post
+from .models import *
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('content',)
+
+class ThreadForm(forms.ModelForm):
+    class Meta:
+        model = Thread
+        fields = ('topic',)
