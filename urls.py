55c6359e129481070e2dfeb257141115f83355d8
ca1ek <ca1ek@users.noreply.github.com>
2015-11-21 01:47:20 +0100

    Added a new quote reply url.

--- urls.py
+++ urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
         url(r'^$', views.forums, name='index'),
         url(r'^forum/(?P<forum_slug>[\w\-]+)/$', views.forum, name='forum'),
         url(r'^thread/(?P<thread_slug>[\w\-]+)/$', views.thread, name='thread'),
-        url(r'^new_post/(?P<thread_slug>[\w\-]+)/$', views.new_post, name='new_post')
+        url(r'^new_post/(?P<thread_slug>[\w\-]+)/$', views.new_post, name='new_post'),
+        url(r'^new_post/(?P<thread_slug>[\w\-]+)/(?P<post_id>[\w\-]+)/$', views.new_post, name='quote_post')
         )
